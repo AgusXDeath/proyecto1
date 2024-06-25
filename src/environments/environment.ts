7c9bf1dc11f399c01e19bf12d2e9257c1cdf32a8
AgusXDeath <santos123agus@gmail.com>
2024-06-25 10:53:25 -0300

    configuración y creación de la estructura

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment= {
+    production: false,
+
+    firebaseConfig:{
+        apiKey: "AIzaSyCGgshGAxwM0nxUziTeduGslg6wgoZHzRk",
+        authDomain: "proyecto-final-5b211.firebaseapp.com",
+        projectId: "proyecto-final-5b211",
+        storageBucket: "proyecto-final-5b211.appspot.com",
+        messagingSenderId: "202070142580",
+        appId: "1:202070142580:web:f1bad3e0965834e5be6b21"
+      }
+}
