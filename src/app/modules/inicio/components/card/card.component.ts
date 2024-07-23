fb449f998391a65d34454a340c4dc212b2402de8
AgusXDeath <santos123agus@gmail.com>
2024-07-23 11:07:18 -0300

    terminando con los retones :v

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -17,7 +17,7 @@ constructor(){
       modelo: "KNU",
       corte: "",
       talle: "40,41,42,43",
-      imagen: "https://assets.esdemarca.com/beta/var/images368/vans-knu-skool-blacktrue-white-vn0009qc6bt1-3036359-b.jpg"
+      imagen: "https://www.asphaltgold.com/cdn/shop/files/6d81647cf487d85f237670b1282064dbc15eb11e_VN0009QC6BT1_Vans_Knu_Skool_Black_True_White_sm_3_768x768_crop_center.jpg?v=1714040467"
     },
     {
       id: "2",
@@ -34,6 +34,62 @@ constructor(){
       corte: "",
       talle: "41,42,44",
       imagen: "https://www.courir.com/dw/image/v2/BCCL_PRD/on/demandware.static/-/Sites-master-catalog-courir/default/dwf8fe43ab/images/hi-res/001514900_105.jpg?sw=600&sh=600&sm=fit&q=90&frz-v=41"
+    },
+    {
+      id: "4",
+      marca:  "Adidas",
+      modelo: "Campus 00's",
+      corte: "",
+      talle: "41,42,44",
+      imagen: "https://di2ponv0v5otw.cloudfront.net/posts/2024/03/19/65f9b46058083d5e54d88725/m_wp_65f9b4c158083dc444d88a17.webp"
+    },
+    {
+      id: "5",
+      marca:  "Dc",
+      modelo: "Pure",
+      corte: "",
+      talle: "41,42,43,44",
+      imagen: "https://do2padres.com/cdn/shop/files/5b911650ddbee9f0c3b5e389bb62b7a4_800x.jpg?v=1697633823"
+    },
+    {
+      id: "6",
+      marca:  "Pants",
+      modelo: "Jean",
+      corte: "Recto, celeste",
+      talle: "M,L",
+      imagen: "https://ae01.alicdn.com/kf/Scb5f44f732914f5bb5c761d735509dfcA.jpg_640x640Q90.jpg_.webp"
+    },
+    {
+      id: "7",
+      marca:  "Pants",
+      modelo: "Jean",
+      corte: "Recto, gris",
+      talle: "S,M,L",
+      imagen: "https://img4.dhresource.com/webp/m/0x0/f3/albu/km/o/31/9c835a8d-7659-44d8-a709-f0ca810755d1.jpg"
+    },
+    {
+      id: "8",
+      marca:  "Pants",
+      modelo: "Carpintero",
+      corte: "Gabardina,verde",
+      talle: "S,M",
+      imagen: "https://ae01.alicdn.com/kf/S48ea28a5b85d409486942b471f5e28f7a/Pantalones-informales-para-hombre-pantal-n-de-moda-holgado-recto-de-pierna-ancha-Retro-Cargo-oto.jpg"
+    },
+    {
+      id: "9",
+      marca:  "Pants",
+      modelo: "Cargo",
+      corte: "Recto, beige",
+      talle: "S,L",
+      imagen: "https://ae01.alicdn.com/kf/S688447089c674549983dc7d388214a82m.jpg_640x640Q90.jpg_.webp"
+    },
+    {
+      id: "10",
+      marca:  "Pants",
+      modelo: "Buzo",
+      corte: "Recto negro",
+      talle: "S,M,L",
+      imagen: "https://ae01.alicdn.com/kf/S2187f2e40e36464fb3436260b5f349c7d/CAAYU-pantalones-de-ch-ndal-rectos-para-hombre-ropa-de-calle-informal-holgada-Harajuku-negra-deportiva.jpg"
     }
   ]
 }
